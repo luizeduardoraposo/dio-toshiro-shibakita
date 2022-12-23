a5ea6d8c32d45f9ecf6a83725b035bb7fcad8858
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2022-12-23 11:44:11 -0300

    Add records in banco.sql

--- banco.sql
+++ banco.sql
@@ -6,3 +6,10 @@ CREATE TABLE dados (
     Cidade varchar(50),
     Host varchar(50)
 );
+CREATE TABLE CLIENTES (
+	id_client INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	Nome VARCHAR(30) NOT NULL,
+	Nascimento DATE NOT NULL,
+	Solteiro CHAR(1) NOT NULL,
+	Email VARCHAR(30),
+        Celular VARCHAR(15)
